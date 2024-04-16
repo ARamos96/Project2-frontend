60ce84fb5d3a1a784154755c30e4028e68c2d50e
Arnau Ramos <arnauramos21@gmail.com>
2024-04-16 18:43:46 +0200

    Created Publisher details page backbone

--- ComicStore/src/pages/PublishersDetailsPage.jsx
+++ ComicStore/src/pages/PublishersDetailsPage.jsx
@@ -1,5 +1,19 @@
 export default function PublishersDetails() {
   return (
-    <div>PublishersDetails</div>
-  )
+    <section className="publisher-details">
+      <div className="publisher-intro">
+        <img />
+        <h1>Publisher Details</h1>
+        <p>Publisher Description</p>
+      </div>
+      {/* <table>Publisher Info Table</table> */}
+      <div className="publisher-history">
+        <h2>Origin</h2>
+        <div className="Timeline">
+          <h3>Timeline</h3>
+          {/* Bulletlist timeline */}
+        </div>
+      </div>
+    </section>
+  );
 }
