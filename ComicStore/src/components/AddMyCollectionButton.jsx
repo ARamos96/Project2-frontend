45cd420944f847d13900ad11e26e6f3e8874dbf9
Abraham Agusti <abraham.agusti@gmail.com>
2024-04-20 16:13:14 +0200

    Commit variopinto

--- ComicStore/src/components/AddMyCollectionButton.jsx
+++ ComicStore/src/components/AddMyCollectionButton.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 function AddMyCollectionButton() {
   return (
     <div>AddMyCollectionButton</div>
--- ComicStore/src/components/AddWishlistButton.jsx
+++ ComicStore/src/components/AddWishlistButton.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 function AddWishlistButton() {
   return (
     <div>AddWishlistButton</div>
