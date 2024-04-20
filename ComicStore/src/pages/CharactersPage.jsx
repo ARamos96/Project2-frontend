d762f5ac206e11ebcd7bff787dd5bf153338a399
Abraham Agusti <abraham.agusti@gmail.com>
2024-04-20 16:55:18 +0200

    Publishers, authors and characters with Pagination

--- ComicStore/src/pages/CharactersPage.jsx
+++ ComicStore/src/pages/CharactersPage.jsx
@@ -1,27 +1,54 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import Pagination from '../components/Pagination';
 
 function CharactersPage() {
 
   const [characters, setCharacters] = useState([]);
   const [searchCharacter, setSearchCharacters] = useState('');
+  const [currentPage,setCurrentPage] = useState(1);
+  const [totalPages,setTotalPages] = useState(0)
 
   useEffect(() => {
+    fetchCharacters();
+  },[currentPage]);
+    
+    
+  const fetchCharacters = () =>{
+    const offset = (currentPage -1) * 100;  
     axios
       .get(
-        'https://corsproxy.io/?https://comicvine.gamespot.com/api/characters/?api_key=14c652d473fc13e73ef42b10edd6423d911d4969&format=json'
-        , {
+        `https://corsproxy.io/?https://comicvine.gamespot.com/api/characters/?api_key=14c652d473fc13e73ef42b10edd6423d911d4969&format=json&offset=${offset}`,
+        {
           withCredentials: false
         }
       )
       .then((response) => {
-        setCharacters(response.data.results);
+
+        const sortedCharacters = response.data.results.sort ((a,b) => a.name.localeCompare(b.name))
+
+        setCharacters(sortedCharacters);
+        const totalPagesCount = Math.ceil(response.data.number_of_total_results / 100);
+        setTotalPages(totalPagesCount);
+        //console.log("Total Pages:", totalPagesCount);
       })
       .catch((error) => {
         console.error("Error 404 Page not found", error);
       })
 
-  }, []);
+  };
+
+  const handleNextPage =() => {
+    if (currentPage < totalPages){
+      setCurrentPage (currentPage +1)
+    }
+  }
+
+  const handlePrevPage = () => {
+    if (currentPage > 1){
+      setCurrentPage(currentPage -1)
+    }
+  }
 
   const filteredCharacters = characters.filter((character) =>
     character.name.toLowerCase().includes(searchCharacter.toLowerCase())
@@ -52,6 +79,14 @@ function CharactersPage() {
           </div>
         ))}
       </div>
+
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onNextPage={handleNextPage}
+        onPrevPage={handlePrevPage}
+      />
+
     </section>
   );
 }
--- ComicStore/src/pages/PublishersPage.jsx
+++ ComicStore/src/pages/PublishersPage.jsx
@@ -1,27 +1,53 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import Pagination from '../components/Pagination';
 
 function PublishersPage() {
 
   const [publishers, setPublishers] = useState([]);
   const [searchPublisher, setSearchPublishers] = useState('');
+  const [currentPage,setCurrentPage] = useState(1);
+  const [totalPages,setTotalPages] = useState(0)
 
-  useEffect(() => {
+  useEffect(() =>{
+    fetchPublishers();
+  },[currentPage]);
+
+  const fetchPublishers =() => {
+    const offset = (currentPage - 1) * 100;
     axios
       .get(
-        'https://corsproxy.io/?https://comicvine.gamespot.com/api/publishers/?api_key=14c652d473fc13e73ef42b10edd6423d911d4969&format=json'
-        , {
+        `https://corsproxy.io/?https://comicvine.gamespot.com/api/publishers/?api_key=14c652d473fc13e73ef42b10edd6423d911d4969&format=json&offset=${offset}`,
+       {
           withCredentials: false
         }
       )
       .then((response) => {
-        setPublishers(response.data.results);
+        
+        const sortedPublishers = response.data.results.sort ((a,b) => a.name.localeCompare(b.name))
+
+        setPublishers(sortedPublishers);
+        const totalPagesCount = Math.ceil(response.data.number_of_total_results / 100);
+        setTotalPages(totalPagesCount);
+        //console.log("Total Pages:", totalPagesCount);
       })
       .catch((error) => {
         console.error("Error 404 Page not found", error);
       })
 
-  }, []);
+  };
+
+  const handleNextPage =() => {
+    if (currentPage < totalPages){
+      setCurrentPage (currentPage +1)
+    }
+  }
+
+  const handlePrevPage = () => {
+    if (currentPage > 1){
+      setCurrentPage(currentPage -1)
+    }
+  }
 
   const filteredPublishers = publishers.filter((publisher) =>
     publisher.name.toLowerCase().includes(searchPublisher.toLowerCase())
@@ -52,10 +78,16 @@ function PublishersPage() {
           </div>
         ))}
       </div>
+
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onNextPage={handleNextPage}
+        onPrevPage={handlePrevPage}
+      />
+
     </section>
   );
 }
-
-//adding pagination from here
 
 export default PublishersPage
