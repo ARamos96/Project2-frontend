ef85e57d4e07c8c4538050022eb17f7afee99d86
Abraham Agusti <abraham.agusti@gmail.com>
2024-04-25 18:52:10 +0200

    my page structure

--- ComicStore/src/pages/MyPage.jsx
+++ ComicStore/src/pages/MyPage.jsx
@@ -1,13 +1,42 @@
 function MyArea() {
   return (
     <div>
-      MyPage
+      <section>
+        <h3>My Profile</h3>
+        <p>Username: Diogo Barros</p>
+        <p>Last connectin: 23/04/2023</p>
+        <p>Avatar</p>
+      </section>
+
+      <section>
+        <p>Total comics</p>
+        <p>Total Wishlist</p>
 
 
+      </section>
+      <section>
+        <div>
+          <h4>My Collection</h4>
+          <div>
 
+          </div>
+        </div>
+        <div>
+          <h4>My wishlist</h4>
+          <div>
+            
+          </div>
+        </div>
+
+      </section>
 
     </div>
   )
 }
 
 export default MyArea
+
+
+
+// https://comickeeperbackendapi.adaptable.app
+
