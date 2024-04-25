c971e039f5f4cffbbb84ae36437bfea984f22758
Arnau Ramos <arnauramos21@gmail.com>
2024-04-25 21:08:33 +0200

    styled character-details-page

--- ComicStore/src/pages/CharacterDetailsPage.jsx
+++ ComicStore/src/pages/CharacterDetailsPage.jsx
@@ -9,8 +9,8 @@ function removeHTMLTagsAndSpecialChars(text) {
   const withoutTagsExceptP = text.replace(/<(?!\/?p\b)[^>]*>/g, "");
   // Replace <p> tags with newline characters
   const withParagraphBreaks = withoutTagsExceptP.replace(/<\/?p>/g, "\n");
-  // Remove special characters except commas and full stops
-  const withoutSpecialChars = withParagraphBreaks.replace(/[^\w\s,.]/g, "");
+  // Remove special characters except commas, hyphens and full stops
+  const withoutSpecialChars = withParagraphBreaks.replace(/[^\w\s,.()#-]/g, "");
   return withoutSpecialChars;
 }
 
@@ -31,55 +31,59 @@ function CharacterDetailsPage() {
       });
   }, []);
 
+  if (character) {
+    console.log(typeof character.results.description);
+  }
+
   return (
-    <section className="character-details">
+    <section>
       {character && (
-        <div>
+        <div className="character-details">
           <div className="character-intro">
-            <img src={character.results.image.icon_url} />
+            <img src={character.results.image.small_url} />
             <div className="character-name-and-intro">
               <h1>{character.results.name}</h1>
               <p>{removeHTMLTagsAndSpecialChars(character.results.deck)}</p>
             </div>
           </div>
           {/* <table>Character Info table</table> */}
-          <p>
-            <b>General Information</b>
-          </p>
-          <table>
-            <tbody>
-              <tr>
-                <th>Super Name:</th>
-                <td>{character.results.name}</td>
-              </tr>
-              <tr>
-                <th>Real Name:</th>
-                <td>{character.results.real_name}</td>
-              </tr>
-              <tr>
-                <th>Publisher:</th>
-                <td>{character.results.publisher.name}</td>
-              </tr>
-              <tr>
-                <th>Creators:</th>
-                <td>
-                  <ul>
+          <div className="genInfo-table">
+            <h3>
+              <b>General Information</b>
+            </h3>
+            <table>
+              <tbody>
+                <tr>
+                  <th>Super Name</th>
+                  <td>{character.results.name}</td>
+                </tr>
+                <tr>
+                  <th>Real Name</th>
+                  <td>{character.results.real_name}</td>
+                </tr>
+                <tr>
+                  <th>Publisher</th>
+                  <td>{character.results.publisher.name}</td>
+                </tr>
+                <tr>
+                  <th>Creators</th>
+                  <td>
                     {character.results.creators.map((creator) => (
                       <li key={creator.id}>{creator.name}</li>
                     ))}
-                  </ul>
-                </td>
-              </tr>
-              {/* <tr>
+                  </td>
+                </tr>
+                {/* <tr>
                 <th>First Appearance:</th>
                 <td>Action Comics #1</td>
               </tr> */}
-              <tr>
-                <th>Appears In:</th>
-                <td>{character.results.count_of_issue_appearances}</td>
-              </tr>
-            </tbody>
-          </table>
+                <tr>
+                  <th>Appears In</th>
+                  <td>{character.results.count_of_issue_appearances} issues</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
           <div className="character-history">
             <h3>Origin</h3>
             <p>
@@ -106,7 +110,7 @@ function CharacterDetailsPage() {
               {removeHTMLTagsAndSpecialChars(
                 character.results.description.substring(
                   character.results.description.indexOf("Character Evolution") +
-                    "Creation".length,
+                    "Character Evolution".length,
                   character.results.description.indexOf("Major Story Arcs")
                 )
               )}
