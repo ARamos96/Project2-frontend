b2281eb25831418f097b0849fba30d53714299b5
Abraham Agusti <abraham.agusti@gmail.com>
2024-04-27 13:05:06 +0200

    Delete button Added

--- ComicStore/src/pages/MyPage.jsx
+++ ComicStore/src/pages/MyPage.jsx
@@ -1,61 +1,77 @@
-import { useEffect, useState } from "react"
-import axios from "axios"
-
+import { useEffect, useState } from "react";
+import axios from "axios";
+import AddMyCollectionButton from "../components/AddMyCollectionButton";
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Stack from '@mui/material/Stack';
 
 function MyArea() {
-
   const [collection, setCollection] = useState([]);
-  const [wishlist, setWishlist] = useState([])
-
+  const [wishlist, setWishlist] = useState([]);
 
   useEffect(() => {
-    axios
-      .get('https://comickeeperbackendapi.adaptable.app/collection')
+    axios.get('https://comickeeperbackendapi.adaptable.app/collection')
       .then((response) => {
         const collectionArray = response.data;
         setCollection(collectionArray);
-      })
-  }, [])
+      });
+  }, []);
+
+  useEffect(() => {
+    axios.get('https://comickeeperbackendapi.adaptable.app/wishlist')
+      .then((response) => {
+        const wishlistArray = response.data;
+        setWishlist(wishlistArray);
+      });
+  }, []);
 
   function countObjectsInArray(array) {
     return array.length;
   }
 
-  useEffect(() => {
-    axios
-      .get('https://comickeeperbackendapi.adaptable.app/wishlist')
-      .then((response) => {
-        const wishlistArray = response.data;
-        setWishlist(wishlistArray)
+  const addToCollectionAndDeleteInWishlist = (comic) => {
+    setCollection([...collection, comic]);
+    setWishlist(wishlist.filter(item => item.id !== comic.id));
+  };
+
+  const removeFromCollectionAndEndpoint = (comicId) => {
+    axios.delete(`https://comickeeperbackendapi.adaptable.app/collection/${comicId}`)
+      .then(() => {
+        setCollection(collection.filter(comic => comic.id !== comicId));
       })
-  }, [])
+      .catch(error => {
+        console.error("Error deleting comic from collection:", error);
+      });
+  };
 
-
+  const removeFromWishlistAndEndpoint = (comicId) => {
+    axios.delete(`https://comickeeperbackendapi.adaptable.app/wishlist/${comicId}`)
+      .then(() => {
+        setWishlist(wishlist.filter(comic => comic.id !== comicId));
+      })
+      .catch(error => {
+        console.error("Error deleting comic from wishlist:", error);
+      });
+  };
 
   return (
     <div>
       <section>
         <h3>My Profile</h3>
         <p>Username: Diogo Barros</p>
-        <p>Last connectin: 23/04/2023</p>
+        <p>Last connection: 23/04/2023</p>
         <p>Avatar</p>
       </section>
 
       <section>
         <h4>My statistic</h4>
   
-
         <h5>Total comics</h5>
-
-
         {countObjectsInArray(collection)}
-
-
         <h5>Total Wishlist</h5>
         {countObjectsInArray(wishlist)}
-
-
       </section>
+
       <section>
         <div>
           <h4>My Collection</h4>
@@ -66,10 +82,20 @@ function MyArea() {
                 <p>{comic.volume_title}</p>
                 <p>{comic.issue_title}</p>
                 <p>{comic.issue_number}</p>
+                <Stack direction="row" spacing={2}>
+                  <Button
+                    color="error"
+                    variant="outlined"
+                    startIcon={<DeleteIcon />}
+                    onClick={() => removeFromCollectionAndEndpoint(comic.id)}>
+                    Delete
+                  </Button>
+              </Stack>
               </div>
             ))}
           </div>
         </div>
+        
         <div>
           <h4>My Wishlist</h4>
           <div>
@@ -79,6 +105,16 @@ function MyArea() {
                 <p>{comic.volume_title}</p>
                 <p>{comic.issue_title}</p>
                 <p>{comic.issue_number}</p>
+                <AddMyCollectionButton comic={comic} addToCollectionAndDeleteInWishlist={addToCollectionAndDeleteInWishlist} />
+                <Stack direction="row" spacing={2}>
+                  <Button
+                  color="error"
+                  variant="outlined"
+                  startIcon={<DeleteIcon />}
+                  onClick={() => removeFromWishlistAndEndpoint(comic.id)}>
+                  Delete
+                </Button>
+              </Stack>
               </div>
             ))}
           </div>
