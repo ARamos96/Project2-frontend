f92e16920608be8667659065e88e4516299695cd
Abraham Agusti <abraham.agusti@gmail.com>
2024-05-01 18:08:47 +0200

    homePageCSS

--- ComicStore/src/pages/HomePage.jsx
+++ ComicStore/src/pages/HomePage.jsx
@@ -1,10 +1,5 @@
 import { Link } from "react-router-dom";
-import libroComico from "../assets/Images/libro-comico.png";
-import escritor from "../assets/Images/escritor.png";
-import cultura from "../assets/Images/cultura-corporativa.png";
-import capa from "../assets/Images/capa.png";
-import leer from "../assets/Images/leer.png";
-import headerPicture from "../assets/Images/Free Vector _ Comic style background.jpg"
+
 
 function HomePage() {
   return (
@@ -13,41 +8,38 @@ function HomePage() {
         <h1>Comic Keeper</h1>
 
       </div>
-      <section className="pages-container">
-        <div className="page-block">
+      <section className="pages-container ">
+        <div className="page-block page-block-comics">
           <Link to="/comics">
-            <img src={libroComico} alt="Comic cover" />
+            
             <button>Comics</button>
           </Link>
         </div>
 
-        <div className="page-block">
+        <div className="page-block page-block-author">
           <Link to="/authors">
-            <img src={escritor} alt="Author cover" />
+            
 
             <button>Authors</button>
           </Link>
         </div>
 
-        <div className="page-block">
+        <div className="page-block page-block-publisher">
           <Link to="/publishers">
-            <img src={cultura} alt="Publisher cover" />
 
             <button>Publishers</button>
           </Link>
         </div>
 
-        <div className="page-block">
+        <div className="page-block page-block-character">
           <Link to="/characters">
-            <img src={capa} alt="Characters cover" />
 
             <button>Characters</button>
           </Link>
         </div>
 
-        <div className="page-block">
+        <div className="page-block page-block-mypage">
           <Link to="/myarea">
-            <img src={leer} alt="Personal Area" />
 
             <button>Personal Area</button>
           </Link>
